67826ee6000857f2588274476f24b0a80311d537
Xiaojie Yuan <yxj0207@gmail.com>
2020-08-10 11:07:18 +0800

    setup: add fonts for macosx

--- setup.sh
+++ setup.sh
@@ -5,9 +5,11 @@ ZTV_ROOT=$PWD
 if [ `uname -s` == 'Darwin' ]; then
   PKG_INSTALL='brew install'
   FIREFOX_PROFILE='Library/Application\ Support/Firefox/Profiles'
+  FONTS_DIR='Library/Fonts'
 else
   PKG_INSTALL='sudo apt -y install'
   FIREFOX_PROFILE='.mozilla/firefox'
+  FONTS_DIR='.fonts'
 fi
 
 git config user.email "yxj@gmail.com"
@@ -46,8 +48,9 @@ vim +PlugInstall +qall
 echo '    StrictHostKeyChecking no' | sudo tee -a /etc/ssh/ssh_config
 echo '    UserKnownHostsFile /dev/null' | sudo tee -a /etc/ssh/ssh_config
 
-sudo cp $ZTV_ROOT/fonts/* /usr/share/fonts
-sudo fc-cache -f
+mkdir -p $FONTS_DIR
+cp $ZTV_ROOT/fonts/* $FONTS_DIR
+fc-cache -f
 
 if [ -d "$FIREFOX_PROFILE" ]; then
   d=$(ls -d "$FIREFOX_PROFILE"/*default*)
