15d3236387854038ab51396a4111d7d62b9b77e3
Xiaojie Yuan <yxj0207@gmail.com>
2020-11-18 21:53:05 -0500

    setup: install fontconfig

--- setup.sh
+++ setup.sh
@@ -31,7 +31,8 @@ $PKG_INSTALL git                \
              global             \
              curl               \
              ack                \
-             cmake
+             cmake              \
+             fontconfig
 
 if [ ! -d .oh-my-zsh ]; then
 	git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh || exit
