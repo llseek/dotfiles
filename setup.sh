2008ff079aeeb033841261b5932438a4c6be4fc5
Xiaojie Yuan <yxj0207@gmail.com>
2020-11-28 18:28:57 +0800

    setup: install bat
    needed by syntax highlight in :Rg preview

--- setup.sh
+++ setup.sh
@@ -38,6 +38,7 @@ $PKG_INSTALL git                \
              lsof               \
              bear               \
              rg                 \
+             bat                \
 
 if [ ! -d .oh-my-zsh ]; then
 	git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh || exit
